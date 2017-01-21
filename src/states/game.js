b3209f34530336d5acc977077c3a910c29d5c727
unknown <peterzylka@gmail.com>
2017-01-20 22:58:27 -0600

    Adding controller to log.

--- src/states/game.js
+++ src/states/game.js
@@ -6,16 +6,32 @@ class Game extends Phaser.State {
 
     create() {
         new PlayerBody(this.game);
-        const right = new PlayerArm(this.game, 0);
-        const left = new PlayerArm(this.game, 180);
+        this.right = new PlayerArm(this.game, 0);
+        this.left = new PlayerArm(this.game, 180);
         for (let i = 0; i < 10; ++i) {
             new EnemyPointy(this.game);
         }
         this.input.onDown.add(this.endGame, this);
+
+        this.game.input.gamepad.start();
+
+        this.pad1 = this.game.input.gamepad.pad1;
+
+        
+    }
+
+    dump() {
+        console.log("LEFTX: " + this.pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X));
+        console.log("LEFTY: " + this.pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y));
+        console.log("RIGHTX: " + this.pad1.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_X));
+        console.log("RIGHTY: " + this.pad1.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_Y));
     }
 
     update() {
-
+      if(this.pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) != 0 ||
+       this.pad1.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_X) != 0) {
+        this.dump();
+      }
     }
 
     endGame() {
