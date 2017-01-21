7c33107f88fc670e502332a7ec7aaa7147864aa8
Evan Cobb <cobb.evan@gmail.com>
2017-01-20 21:11:22 -0600

    loading the assets I made

--- src/states/preloader.js
+++ src/states/preloader.js
@@ -1,34 +1,37 @@
 class Preloader extends Phaser.State {
 
-  constructor() {
-    super();
-    this.asset = null;
-    this.ready = false;
-  }
+    constructor() {
+        super();
+        this.asset = null;
+        this.ready = false;
+    }
 
-  preload() {
-    //setup loading bar
-    this.asset = this.add.sprite(this.game.width * 0.5 - 110, this.game.height * 0.5 - 10, 'preloader');
-    this.load.setPreloadSprite(this.asset);
+    preload() {
+        //setup loading bar
+        this.asset = this.add.sprite(this.game.width * 0.5 - 110, this.game.height * 0.5 - 10, 'preloader');
+        this.load.setPreloadSprite(this.asset);
 
-    //Setup loading and its events
-    //this.load.onLoadComplete.addOnce(this.onLoadComplete, this);
-    //this.loadResources();
-  }
+        Setup loading and its events
+        this.load.onLoadComplete.addOnce(this.onLoadComplete, this);
+        this.loadResources();
+    }
 
-  update() {
-      // if (this.ready) {
-        this.game.state.start('menu');
-      // }
-  }
+    update() {
+        if (this.ready) {
+            this.game.state.start('menu');
+        }
+    }
 
-  loadResources() {
-      // load your resources here
-  }
+    loadResources() {
+        // load your resources here
+        game.load.image('enemy', 'assets/enemyPointy.png');
+        game.load.image('playerArm', 'assets/playerArm.png');
+        game.load.image('playerBody', 'assets/playerBody.png');
+    }
 
-  onLoadComplete() {
-    this.ready = true;
-  }
+    onLoadComplete() {
+        this.ready = true;
+    }
 }
 
 export default Preloader;
