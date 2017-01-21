299ef93468e424b4c2e21f85bacc7ee3c7f6b832
Becky Asbury <rasbury7@gmail.com>
2017-01-21 13:48:10 -0600

    Enemies move around randomly, and only maybe will head towards player

--- src/prefabs/enemyPointy.js
+++ src/prefabs/enemyPointy.js
@@ -7,12 +7,46 @@ class EnemyPointy extends Phaser.Sprite {
         game.add.existing(this);
         this.Player = player;
         this.anchor.setTo(0.5,0.5);
+
+        this.movementAction = "Random";
+        this.targetX = player.x;
+        this.targetY = player.y;
+          console.log( this.targetX, this.targetY);
+
+        //this.tween = game.add.tween(this).to( { x: 200 }, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true);
+
+         //  When the tween loops it decides what move to make
+         //this.tween.onLoop.add(this.makeMove, this);
+
     }
 
     //Code ran on each frame of game
     update() {
 
-      this.game.physics.arcade.moveToObject(this, this.Player,120);
+      this.makeMove();
+    }
+
+    makeMove(){
+      console.log("MakeMove called", this.targetX, this.targetY);
+
+      let courseChange = this.game.rnd.integerInRange(0, 100);
+      courseChange = courseChange - 50;
+
+      this.targetY = this.targetY + courseChange;
+      this.targetX = this.targetX + courseChange;
+
+      this.game.physics.arcade.moveToXY(this, this.targetX, this.targetY, 120);
+
+        //if (this.movementAction == "Random"){
+        //  this.body.velocity.x = 0;
+        //    this.body.velocity.y = 0;
+        //    this.y+=10;
+        //    this.movementAction = "Focused";
+        //  }
+        //else {
+        //    this.game.physics.arcade.moveToObject(this, this.Player,120);
+
+      //}
 
     }
 
