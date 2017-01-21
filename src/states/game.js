a18164c09c505d1216d4fcb388a15e5a9a906bf5
Evan Cobb <cobb.evan@gmail.com>
2017-01-20 22:33:31 -0600

    for convenience, enemies spawn in the line of fire

--- src/states/game.js
+++ src/states/game.js
@@ -10,16 +10,16 @@ class Game extends Phaser.State {
         const right = new PlayerArm(this.game, 0);
         const left = new PlayerArm(this.game, 180);
         for (let i = 0; i < 10; ++i) {
-            const minimumEdgeBuffer = 100;
-            let x = this.game.rnd.integerInRange(minimumEdgeBuffer, this.game.world.width / 2 - minimumEdgeBuffer);
-            if (x > this.game.world.width / 4) {
-                x += this.game.world.width / 2;
-            }
-            let y = this.game.rnd.integerInRange(minimumEdgeBuffer, this.game.world.height / 2 - minimumEdgeBuffer);
-            if (y > this.game.world.height / 4) {
-                y += this.game.world.height / 2;
-            }
-            new EnemyPointy(this.game, x, y);
+            // const minimumEdgeBuffer = 100;
+            // let x = this.game.rnd.integerInRange(minimumEdgeBuffer, this.game.world.width / 2 - minimumEdgeBuffer);
+            // if (x > this.game.world.width / 4) {
+            //     x += this.game.world.width / 2;
+            // }
+            // let y = this.game.rnd.integerInRange(minimumEdgeBuffer, this.game.world.height / 2 - minimumEdgeBuffer);
+            // if (y > this.game.world.height / 4) {
+            //     y += this.game.world.height / 2;
+            // }
+            new EnemyPointy(this.game, this.player.x + 200 + i, this.player.y);
         }
         this.game.input.onDown.add(() => {
             this.shoot();
