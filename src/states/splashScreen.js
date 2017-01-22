033692d351504db7953716704e9b509801363b2d
Evan Cobb <cobb.evan@gmail.com>
2017-01-22 15:19:52 -0600

    added studio logo

--- src/states/splashScreen.js
+++ src/states/splashScreen.js
@@ -13,7 +13,36 @@ class SplashScreen extends Phaser.State {
         ).yoyo(true, 1000);
 
         tween.onComplete.add(() => {
-            this.game.state.start('menu');
+            const logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'studio');
+            logo.anchor.set(0.5);
+            logo.scale.set(2);
+
+            logo.animations.add('logo');
+            const animation = logo.animations.play('logo', 15, false);
+
+            animation.onComplete.add(() => {
+                const style = {
+                    font: '42px Arial',
+                    fill: '#ffffff',
+                    stroke: 0x333333,
+                    strokeThickness: 5,
+                    align: 'center'
+                };
+                const text = this.game.add.text(this.game.world.centerX, 100, 'Jokes 2 Far', style);
+                text.alpha = 0;
+                text.anchor.set(0.5);
+                const txtTween = this.game.add.tween(text).to(
+                    {alpha:1},
+                    2000,
+                    Phaser.Easing.Linear.In,
+                    true
+                );
+
+                txtTween.onComplete.add(() => {
+                    this.game.state.start('menu');
+                });
+
+            });
         });
     }
 
