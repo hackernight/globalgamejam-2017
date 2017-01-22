da3d3b934d36c8701f6970d6af6451b48fe903c5
unknown <peterzylka@gmail.com>
2017-01-21 20:54:54 -0600

    Adding continue.

--- src/states/gameover.js
+++ src/states/gameover.js
@@ -16,6 +16,8 @@ class Menu extends Phaser.State {
       message = "Officer Waverton failed\n in his quest for justice.\n The city remains in danger\n from the Gang of 99."
     }
 
+    message += "\n(Press X to continue)"
+
     var style = { font: "65px Arial", fill: "#ffffff", align: "center" };
     var text = this.add.text(this.game.world.centerX, this.game.world.centerY, message, style);
     text.anchor.set(0.5);
